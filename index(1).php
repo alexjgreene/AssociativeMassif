fc15b61389be738377eadcd408285dc70f175a56
Abramovichvitaly <abramovich1997@yandex.ru>
2017-10-13 15:40:11 +0700

    Максимальный средний балл

---
+++ index(1).php
@@ -0,0 +1,17 @@
+<?php
+$groups=[
+'101'=> 4.2,
+'102' => 4.0,
+'103'=> 4.8,
+'104' => 4.6,
+'105' => 3.8
+];
+
+$max=0;
+foreach ($groups as $groupNumber=> $value){
+	if($max<$value){
+		$max=$value;
+	}
+}
+echo '<p> Максимальный средний балл: '.$max.'</p>';
+?>
